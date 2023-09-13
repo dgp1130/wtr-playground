929f9f4e98c5eec8ab81e3f5a7d5d433e5e34776
Doug Parker <dgp1130@users.noreply.github.com>
2023-09-13 14:25:12 -0700

    Adds NPM dependency.

--- src/bar.ts
+++ src/bar.ts
@@ -1,5 +1,15 @@
+import { LitElement, TemplateResult, html } from 'lit';
+import { customElement } from 'lit/decorators.js';
+
 export function bar(): string {
   return 'bar';
 }
 
 export let prop = 'test';
+
+@customElement('my-element')
+class MyElement extends LitElement {
+  protected render(): TemplateResult {
+    return html`<div>Hello, World!</div>`;
+  }
+}
